66bf9fc78ad3fb225c20bed15447a36a009d4c3b
estudiante <estudiante@DESKTOP-OPSU5DS>
2024-08-26 21:59:50 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio3.entidades/Cilindro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CilindroConsola;
+
+namespace Entidades
+{
+    public struct Cilindro
+    {
+        public double radio;
+        public double altura;
+
+        public Cilindro(double radio, double altura)
+        {
+            var radio == radio;
+            var altura == altura;
+        }
+
+        public double GetRadio()
+        {
+            return radio;
+        }
+
+        public void SetRadio(double radio)
+        {
+            var radio == radio;
+        }
+
+        public double GetAltura()
+        {
+            return altura;
+        }
+
+        public void SetAltura(double altura)
+        {
+            var altura == altura;
+        }
+
+        public double CalcularArea()
+        {
+            return 2 * Math.PI * radio * (radio + altura);
+        }
+
+        public double CalcularVolumen()
+        {
+            return Math.PI * Math.Pow(radio, 2) * altura;
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cilindro;
+
+namespace CilindroConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cilindro cilindro = new Cilindro();
+
+            Console.Write("Ingrese el radio del cilindro: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+            cilindro.SetRadio(radio);
+
+            Console.Write("Ingrese la altura del cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            cilindro.SetAltura(altura);
+
+            Console.WriteLine("El área del cilindro es: " + cilindro.CalcularArea());
+            Console.WriteLine("El volumen del cilindro es: " + cilindro.CalcularVolumen());
+
+            Console.ReadKey();
+        }
+    }
+}
